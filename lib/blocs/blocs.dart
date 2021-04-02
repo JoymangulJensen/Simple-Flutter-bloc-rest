d7e3a93c0d6eb44b45a35e3ab272aa348a16eeb5
Younes OUFRID <oufridyounes@gmail.com>
2021-04-02 13:14:51 +0100

    🔨 Add blocs file for exportation

--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -1,3 +1,5 @@
+export 'co2/co2_event.dart';
+export 'co2/co2_state.dart';
 export 'temperature/temperature_event.dart';
 export 'temperature/temperature_state.dart';
 export 'temperature/temperature_event.dart';
