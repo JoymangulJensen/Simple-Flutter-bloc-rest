b01c82664f14f6caf6c0ec8f5829f7c24c6e907b
hajarAtifi <atifihajar332@gmail.com>
2021-04-07 10:19:55 +0100

    🔨Update and finalize bloc

--- lib/blocs/bloc/temperature/temperature_event.dart
+++ lib/blocs/bloc/temperature/temperature_event.dart
@@ -1,12 +1,14 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 abstract class TemperatureEvent extends Equatable {
   const TemperatureEvent();
 }
 
 class TemperatureRequested extends TemperatureEvent {
-  const TemperatureRequested();
 
+  final id  ;
+  const TemperatureRequested({@required this.id});
   @override
-  List<Object> get props => [];
+  List<Object> get props => [id];
 }
