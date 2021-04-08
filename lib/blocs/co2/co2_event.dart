2def014455e5d7aa34befd89503dbfa32a5959ea
Younes OUFRID <oufridyounes@gmail.com>
2021-04-08 14:25:00 +0100

    🔨 Update co2 event
    updating co2 event dart file

--- lib/blocs/co2/co2_event.dart
+++ lib/blocs/co2/co2_event.dart
@@ -1,13 +1,14 @@
-import 'package:equatable/equatable.dart';
+part of 'co2_bloc.dart';
 
 abstract class Co2Event extends Equatable {
   const Co2Event();
 }
 
 class Co2Requested extends Co2Event {
-
-  const Co2Requested();
+  final id;
+  const Co2Requested({@required this.id});
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [id];
 }
+
