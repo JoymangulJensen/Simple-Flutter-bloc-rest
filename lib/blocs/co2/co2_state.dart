f6c8ac46ff90afb07f72954c7a50460cd8b1da2e
Younes OUFRID <oufridyounes@gmail.com>
2021-04-08 14:25:19 +0100

    🔨 Update co2 state
    Updating co2 state dart file

--- lib/blocs/co2/co2_state.dart
+++ lib/blocs/co2/co2_state.dart
@@ -1,20 +1,21 @@
-import 'package:meta/meta.dart';
-import 'package:equatable/equatable.dart';
-import 'package:poc_fo/models/models.dart';
+part of 'co2_bloc.dart';
 
 abstract class Co2State extends Equatable {
   const Co2State();
+}
 
+class Co2Initial extends Co2State {
   @override
   List<Object> get props => [];
 }
 
-class Co2Initial extends Co2State {}
-
-class Co2LoadInProgress extends Co2State {}
+class Co2LoadInProgress extends Co2State {
+  @override
+  List<Object> get props => [];
+}
 
 class Co2LoadSuccess extends Co2State {
-  final List<Co2> co2;
+  final Co2 co2;
 
   const Co2LoadSuccess({@required this.co2}) : assert(co2 != null);
 
@@ -22,4 +23,7 @@ class Co2LoadSuccess extends Co2State {
   List<Object> get props => [co2];
 }
 
-class Co2LoadFailure extends Co2State {}
+class Co2LoadFailure extends Co2State {
+  @override
+  List<Object> get props => [];
+}
