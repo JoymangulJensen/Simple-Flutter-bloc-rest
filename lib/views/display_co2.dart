489d4dd6e1b0fb62e3828f57dadbdf62ea54d5a0
Younes OUFRID <oufridyounes@gmail.com>
2021-04-08 14:28:49 +0100

    ✨Add display one co2 data view
    the view to show one co2 data information

---
+++ lib/views/display_co2.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poc_fo/blocs/blocs.dart';
+
+class ShowPatient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Display Co2 data"),
+      ),
+      body: Center(
+        child: BlocBuilder<Co2Bloc, Co2State>(
+          builder: (context, state) {
+            if (state is Co2Initial) {
+              BlocProvider.of<Co2Bloc>(context)
+                  .add(Co2Requested(id: 1));
+            }
+            if (state is Co2LoadSuccess) {
+              return Text(state.co2.toJson().toString());
+            }
+            if (state is Co2LoadFailure) {
+              return Text('Co2 data not loaded');
+            }
+            return Column(
+              children: [
+                CircularProgressIndicator(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
