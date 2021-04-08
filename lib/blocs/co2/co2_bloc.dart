4dd9e5e25d6bb38fc4723abbc36900bdd117aaf2
Younes OUFRID <oufridyounes@gmail.com>
2021-04-08 14:24:38 +0100

    🔨 Update co2 bloc
    updating Co2 bloc dart file

--- lib/blocs/co2/co2_bloc.dart
+++ lib/blocs/co2/co2_bloc.dart
@@ -1,9 +1,13 @@
-import 'package:meta/meta.dart';
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 import 'package:poc_fo/models/models.dart';
 import 'package:poc_fo/repositories/repositories.dart';
-import 'package:poc_fo/blocs/blocs.dart';
+
+part 'co2_event.dart';
+part 'co2_state.dart';
 
 class Co2Bloc extends Bloc<Co2Event, Co2State> {
   final Co2Repository co2Repository;
@@ -17,9 +21,8 @@ class Co2Bloc extends Bloc<Co2Event, Co2State> {
     if (event is Co2Requested) {
       yield Co2LoadInProgress();
       try {
-
-        final List<Co2> co2list = await co2Repository.GetCo2List();
-        yield Co2LoadSuccess(co2: co2list);
+        final Co2 co2 = await co2Repository.GetOneCo2(id: event.id);
+        yield Co2LoadSuccess(co2: co2);
       } catch (_) {
         yield Co2LoadFailure();
       }
